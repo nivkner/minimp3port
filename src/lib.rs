cbcf4178f2b689c70423bfb984cffc1b79b086f2
Niv Kaminer <nivkner@zoho.com>
2018-10-20 19:46:06 +0300

    add test to hdr_bitrate_kbps

--- src/lib.rs
+++ src/lib.rs
@@ -428,4 +428,20 @@ mod tests {
             hdr_frame_samples(&native_hdr) as i64 == unsafe { ffi::hdr_frame_samples(ffi_hdr.as_ptr()) as i64 }
         }
     }
+
+    quickcheck! {
+        fn test_hdr_bitrate_kbps(data: Vec<u8>) -> bool {
+            if data.len() == 0 {
+                return true // empty data is not interesting
+            }
+            let mut native_hdr = Vec::new();
+            native_hdr.extend(data.into_iter().cycle().take(HDR_SIZE as _));
+            if !hdr_valid(&native_hdr) {
+                return true; // function should only be used on valid headers
+            }
+            let ffi_hdr = native_hdr.clone();
+            // ensure values stay the same for both funtions (i64 covers i32 and u32)
+            hdr_bitrate_kbps(&native_hdr) as i64 == unsafe { ffi::hdr_bitrate_kbps(ffi_hdr.as_ptr()) as i64 }
+        }
+    }
 }
