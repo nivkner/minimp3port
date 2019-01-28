c717db31f4e2e395e9cfe558db4f73fd99fb20ae
Niv Kaminer <nivkner@zoho.com>
2019-01-28 23:04:23 +0200

    add data to decoder that can affect execution in fuzzer

--- mp3fuzz/src/main.rs
+++ mp3fuzz/src/main.rs
@@ -4,10 +4,21 @@ use minimp3port::{decode_frame, MINIMP3_MAX_SAMPLES_PER_FRAME};
 fn main() {
     let mut pcm = [0; MINIMP3_MAX_SAMPLES_PER_FRAME as usize];
     let mut info = unsafe { std::mem::zeroed() };
-    let mut decoder = unsafe { std::mem::zeroed() };
+    let mut decoder: minimp3port::mp3dec_t = unsafe { std::mem::zeroed() };
 
     loop {
         fuzz!(|data: &[u8]| {
+            let mut repeat = data.iter().rev().cycle().cloned();
+            repeat
+                .by_ref()
+                .zip(decoder.header.iter_mut())
+                .for_each(|(from, to)| *to = from);
+            let mut bytes = [0u8; 4];
+            repeat
+                .by_ref()
+                .zip(bytes.iter_mut())
+                .for_each(|(from, to)| *to = from);
+            decoder.free_format_bytes = i32::from_le_bytes(bytes);
             decode_frame(&mut decoder, &data, Some(&mut pcm), &mut info);
         });
     }
