c402920b369f995ae8cd268e508c358e58eef808
Niv Kaminer <nivkner@zoho.com>
2019-01-30 00:52:17 +0200

    Revert "catch more errors by checking whether a test was meant to fail"
    This reverts commit 61d49d3feba3bdadcb76ef7575c279cf57985e8e.

--- tests/psnr_compliance.rs
+++ tests/psnr_compliance.rs
@@ -11,7 +11,7 @@ macro_rules! test_all {
         // Start a repetition:
         $(
             // each repeat has the name of te module and the name of the files without extentions
-            $element:ident: $string:literal - $fail:literal
+            $element:ident: $string:expr
         )
         // ...separated by commas...
         ,
@@ -25,7 +25,7 @@ macro_rules! test_all {
 
             #[test]
             fn test() {
-                super::decode(MP3, PCM, $fail);
+                super::decode(MP3, PCM);
             }
         }
         )*
@@ -33,77 +33,77 @@ macro_rules! test_all {
 }
 
 test_all![
-    ILL2_center2: "ILL2_center2" - true,
-    ILL2_dual: "ILL2_dual" - true,
-    ILL2_dynx22: "ILL2_dynx22" - true,
-    ILL2_dynx31: "ILL2_dynx31" - true,
-    ILL2_dynx32: "ILL2_dynx32" - true,
-    ILL2_ext_switching: "ILL2_ext_switching" - false,
-    ILL2_layer1: "ILL2_layer1" - false,
-    ILL2_layer3: "ILL2_layer3" - false,
-    ILL2_mono: "ILL2_mono" - true,
-    ILL2_multilingual: "ILL2_multilingual" - true,
-    ILL2_overalloc1: "ILL2_overalloc1" - true,
-    ILL2_overalloc2: "ILL2_overalloc2" - true,
-    ILL2_prediction: "ILL2_prediction" - true,
-    ILL2_samples: "ILL2_samples" - true,
-    ILL2_scf63: "ILL2_scf63" - true,
-    ILL2_tca21: "ILL2_tca21" - true,
-    ILL2_tca30: "ILL2_tca30" - true,
-    ILL2_tca30_PC: "ILL2_tca30_PC" - true,
-    ILL2_tca31_PC: "ILL2_tca31_PC" - true,
-    ILL2_tca31_mtx0: "ILL2_tca31_mtx0" - true,
-    ILL2_tca31_mtx2: "ILL2_tca31_mtx2" - true,
-    ILL2_tca32_PC: "ILL2_tca32_PC" - true,
-    ILL2_wrongcrc: "ILL2_wrongcrc" - true,
-    ILL4_ext_id1: "ILL4_ext_id1" - true,
-    ILL4_sync: "ILL4_sync" - true,
-    ILL4_wrong_length1: "ILL4_wrong_length1" - true,
-    ILL4_wrong_length2: "ILL4_wrong_length2" - true,
-    ILL4_wrongcrc: "ILL4_wrongcrc" - true,
-    M2L3_bitrate_16_all: "M2L3_bitrate_16_all" - false,
-    M2L3_bitrate_22_all: "M2L3_bitrate_22_all" - false,
-    M2L3_bitrate_24_all: "M2L3_bitrate_24_all" - false,
-    M2L3_compl24: "M2L3_compl24" - false,
-    M2L3_noise: "M2L3_noise" - false,
-    l1_fl1: "l1-fl1" - false,
-    l1_fl2: "l1-fl2" - false,
-    l1_fl3: "l1-fl3" - false,
-    l1_fl4: "l1-fl4" - false,
-    l1_fl5: "l1-fl5" - false,
-    l1_fl6: "l1-fl6" - false,
-    l1_fl7: "l1-fl7" - false,
-    l1_fl8: "l1-fl8" - false,
-    l2_fl10: "l2-fl10" - false,
-    l2_fl11: "l2-fl11" - false,
-    l2_fl12: "l2-fl12" - false,
-    l2_fl13: "l2-fl13" - false,
-    l2_fl14: "l2-fl14" - false,
-    l2_fl15: "l2-fl15" - false,
-    l2_fl16: "l2-fl16" - false,
-    l2_nonstandard_fl1_fl2_ff: "l2-nonstandard-fl1_fl2_ff" - false,
-    l2_nonstandard_free_format: "l2-nonstandard-free_format" - false,
-    l2_nonstandard_test32_size: "l2-nonstandard-test32-size" - false,
-    l2_test32: "l2-test32" - false,
-    l3_compl: "l3-compl" - false,
-    l3_he_32khz: "l3-he_32khz" - false,
-    l3_he_44khz: "l3-he_44khz" - false,
-    l3_he_48khz: "l3-he_48khz" - false,
-    l3_he_free: "l3-he_free" - false,
-    l3_he_mode: "l3-he_mode" - false,
-    l3_hecommon: "l3-hecommon" - false,
-    l3_id3v2: "l3-id3v2" - true,
-    l3_nonstandard_big_iscf: "l3-nonstandard-big-iscf" - true,
-    l3_nonstandard_compl_sideinfo_bigvalues: "l3-nonstandard-compl-sideinfo-bigvalues" - false,
-    l3_nonstandard_compl_sideinfo_blocktype: "l3-nonstandard-compl-sideinfo-blocktype" - false,
-    l3_nonstandard_compl_sideinfo_size: "l3-nonstandard-compl-sideinfo-size" - false,
-    l3_nonstandard_sideinfo_size: "l3-nonstandard-sideinfo-size" - true,
-    l3_si: "l3-si" - false,
-    l3_si_block: "l3-si_block" - false,
-    l3_si_huff: "l3-si_huff" - false,
-    l3_sin1k0db: "l3-sin1k0db" - false,
-    l3_test45: "l3-test45" - false,
-    l3_test46: "l3-test46" - false
+    ILL2_center2: "ILL2_center2",
+    ILL2_dual: "ILL2_dual",
+    ILL2_dynx22: "ILL2_dynx22",
+    ILL2_dynx31: "ILL2_dynx31",
+    ILL2_dynx32: "ILL2_dynx32",
+    ILL2_ext_switching: "ILL2_ext_switching",
+    ILL2_layer1: "ILL2_layer1",
+    ILL2_layer3: "ILL2_layer3",
+    ILL2_mono: "ILL2_mono",
+    ILL2_multilingual: "ILL2_multilingual",
+    ILL2_overalloc1: "ILL2_overalloc1",
+    ILL2_overalloc2: "ILL2_overalloc2",
+    ILL2_prediction: "ILL2_prediction",
+    ILL2_samples: "ILL2_samples",
+    ILL2_scf63: "ILL2_scf63",
+    ILL2_tca21: "ILL2_tca21",
+    ILL2_tca30: "ILL2_tca30",
+    ILL2_tca30_PC: "ILL2_tca30_PC",
+    ILL2_tca31_PC: "ILL2_tca31_PC",
+    ILL2_tca31_mtx0: "ILL2_tca31_mtx0",
+    ILL2_tca31_mtx2: "ILL2_tca31_mtx2",
+    ILL2_tca32_PC: "ILL2_tca32_PC",
+    ILL2_wrongcrc: "ILL2_wrongcrc",
+    ILL4_ext_id1: "ILL4_ext_id1",
+    ILL4_sync: "ILL4_sync",
+    ILL4_wrong_length1: "ILL4_wrong_length1",
+    ILL4_wrong_length2: "ILL4_wrong_length2",
+    ILL4_wrongcrc: "ILL4_wrongcrc",
+    M2L3_bitrate_16_all: "M2L3_bitrate_16_all",
+    M2L3_bitrate_22_all: "M2L3_bitrate_22_all",
+    M2L3_bitrate_24_all: "M2L3_bitrate_24_all",
+    M2L3_compl24: "M2L3_compl24",
+    M2L3_noise: "M2L3_noise",
+    l1_fl1: "l1-fl1",
+    l1_fl2: "l1-fl2",
+    l1_fl3: "l1-fl3",
+    l1_fl4: "l1-fl4",
+    l1_fl5: "l1-fl5",
+    l1_fl6: "l1-fl6",
+    l1_fl7: "l1-fl7",
+    l1_fl8: "l1-fl8",
+    l2_fl10: "l2-fl10",
+    l2_fl11: "l2-fl11",
+    l2_fl12: "l2-fl12",
+    l2_fl13: "l2-fl13",
+    l2_fl14: "l2-fl14",
+    l2_fl15: "l2-fl15",
+    l2_fl16: "l2-fl16",
+    l2_nonstandard_fl1_fl2_ff: "l2-nonstandard-fl1_fl2_ff",
+    l2_nonstandard_free_format: "l2-nonstandard-free_format",
+    l2_nonstandard_test32_size: "l2-nonstandard-test32-size",
+    l2_test32: "l2-test32",
+    l3_compl: "l3-compl",
+    l3_he_32khz: "l3-he_32khz",
+    l3_he_44khz: "l3-he_44khz",
+    l3_he_48khz: "l3-he_48khz",
+    l3_he_free: "l3-he_free",
+    l3_he_mode: "l3-he_mode",
+    l3_hecommon: "l3-hecommon",
+    l3_id3v2: "l3-id3v2",
+    l3_nonstandard_big_iscf: "l3-nonstandard-big-iscf",
+    l3_nonstandard_compl_sideinfo_bigvalues: "l3-nonstandard-compl-sideinfo-bigvalues",
+    l3_nonstandard_compl_sideinfo_blocktype: "l3-nonstandard-compl-sideinfo-blocktype",
+    l3_nonstandard_compl_sideinfo_size: "l3-nonstandard-compl-sideinfo-size",
+    l3_nonstandard_sideinfo_size: "l3-nonstandard-sideinfo-size",
+    l3_si: "l3-si",
+    l3_si_block: "l3-si_block",
+    l3_si_huff: "l3-si_huff",
+    l3_sin1k0db: "l3-sin1k0db",
+    l3_test45: "l3-test45",
+    l3_test46: "l3-test46"
 ];
 
 struct mp3dec_file_info_t {
@@ -235,7 +235,7 @@ fn mse(samples: usize, frame_buf: &[i16], buf_ref: &[i16]) -> (f64, i32) {
     (MSE, maxdiff)
 }
 
-fn decode(input_buffer: &[u8], buf: &[u8], should_fail: bool) {
+fn decode(input_buffer: &[u8], buf: &[u8]) {
     let mut mp3d: mp3dec_t = mp3dec_t {
         mdct_overlap: [[0.; 288]; 2],
         qmf_state: [0.; 960],
@@ -260,18 +260,17 @@ fn decode(input_buffer: &[u8], buf: &[u8], should_fail: bool) {
     } else {
         1.0
     };
-    if MSE == 0.0 {
-        assert!(should_fail);
+    let psnr = if MSE == 0.0 {
+        99.0
     } else {
-        assert!(!should_fail);
-        let psnr = 10.0 * (0x7fffu32.pow(2) as f64 / MSE).log10();
-        assert!(
-            psnr > 96.0,
-            "PSNR compliance failed: rate={} samples={} max_diff={} PSNR={}",
-            info.hz,
-            info.samples,
-            maxdiff,
-            psnr
-        );
-    }
+        10.0 * (0x7fffu32.pow(2) as f64 / MSE).log10()
+    };
+    assert!(
+        psnr > 96.0,
+        "PSNR compliance failed: rate={} samples={} max_diff={} PSNR={}",
+        info.hz,
+        info.samples,
+        maxdiff,
+        psnr
+    )
 }
